37bb0946e3ab163d0d4519c595d2bec02f5bd1c3
MNNB <fdecarpentrie@gmail.com>
2024-06-11 23:02:24 +0200

    add the js code for the calculation

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const convertBtn = document.getElementById("convert-btn")
 let inputEl = document.getElementById("input-el")
+let lengthEl = document.getElementById("length-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
 
 convertBtn.addEventListener("click", function() {
     console.log("clicked")
     console.log(inputEl.value)
 
-    let lengthResult = +inputEl.value * 3.28084
-    let volumeResult = +inputEl.value / 3.785
-    let massResult = +inputEl.value * 2.2046
+    let lengthResultFeet = +inputEl.value * 3.28084
+    let lengthResultMeters = +inputEl.value / 3.28084
+    let volumeResultGallons = +inputEl.value / 3.785
+    let volumeResultLiters = +inputEl.value * 3.785
+    let massResultPounds = +inputEl.value * 2.2046
+    let massResultKilos = +inputEl.value / 2.2046
 
-    inputEl.value = ""
+    lengthEl.innerHTML = `<p>${inputEl.value} meters = ${lengthResultFeet.toFixed(3)} feet | ${inputEl.value} feet = ${lengthResultMeters.toFixed(3)} meters</p>`
 
-    console.log(lengthResult.toFixed(3))
-    console.log(volumeResult.toFixed(3))
-    console.log(massResult.toFixed(3))
+    volumeEl.innerHTML = `<p>${inputEl.value} gallons = ${volumeResultGallons.toFixed(3)} liters | ${inputEl.value} liters = ${volumeResultLiters.toFixed(3)} gallons</p>`
+
+    massEl.innerHTML = `<p>${inputEl.value} kilos = ${massResultPounds.toFixed(3)} pounds | ${inputEl.value} pounds = ${massResultKilos.toFixed(3)} kilos</p>`
+
 })
