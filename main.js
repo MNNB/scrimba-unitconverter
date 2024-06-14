d6677a9697e804f45df6e7775d190a8b99d8c125
MNNB <fdecarpentrie@gmail.com>
2024-06-14 22:41:43 +0200

    add dark/light mode mechanics

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const convertBtn = document.getElementById("convert-btn")
+const modeBtn = document.getElementById('mode')
+
 let inputEl = document.getElementById("input-el")
 let lengthEl = document.getElementById("length-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
 
 convertBtn.addEventListener("click", function() {
-    console.log("clicked")
-    console.log(inputEl.value)
+    // console.log("clicked")
+    // console.log(inputEl.value)
 
     let lengthResultFeet = +inputEl.value * 3.28084
     let lengthResultMeters = +inputEl.value / 3.28084
@@ -22,3 +24,31 @@ convertBtn.addEventListener("click", function() {
     massEl.innerHTML = `<p>${inputEl.value} kilos = ${massResultPounds.toFixed(3)} pounds | ${inputEl.value} pounds = ${massResultKilos.toFixed(3)} kilos</p>`
 
 })
+
+// Light/Dark mode
+// from this tutorial https://dcodemania.com/post/switch-dark-light-mode-css-javascript
+
+modeBtn.onchange = (e) => {
+    if (modeBtn.checked === true) {
+      document.documentElement.classList.remove("light")
+      document.documentElement.classList.add("dark")
+      window.localStorage.setItem('mode', 'dark');
+    } else {
+      document.documentElement.classList.remove("dark")
+      document.documentElement.classList.add("light")
+      window.localStorage.setItem('mode', 'light');
+    }
+  }
+
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') {
+  modeBtn.checked = true;
+  document.documentElement.classList.remove("light")
+  document.documentElement.classList.add("dark")
+}
+
+if (mode == 'light') {
+  modeBtn.checked = false;
+  document.documentElement.classList.remove("dark")
+  document.documentElement.classList.add("light")
+}
