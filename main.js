25b4f02c49ab21864a9579baded37fa784115752
MNNB <fdecarpentrie@gmail.com>
2024-06-15 11:31:11 +0200

    change light/dark color theme and fix alignment in the results cards

--- main.js
+++ main.js
@@ -17,11 +17,14 @@ convertBtn.addEventListener("click", function() {
     let massResultPounds = +inputEl.value * 2.2046
     let massResultKilos = +inputEl.value / 2.2046
 
-    lengthEl.innerHTML = `<p>${inputEl.value} meters = ${lengthResultFeet.toFixed(3)} feet | ${inputEl.value} feet = ${lengthResultMeters.toFixed(3)} meters</p>`
+    lengthEl.innerHTML = `<p>${inputEl.value} meters = ${lengthResultFeet.toFixed(3)} feet</p>
+                          <p>${inputEl.value} feet = ${lengthResultMeters.toFixed(3)} meters</p>`
 
-    volumeEl.innerHTML = `<p>${inputEl.value} gallons = ${volumeResultGallons.toFixed(3)} liters | ${inputEl.value} liters = ${volumeResultLiters.toFixed(3)} gallons</p>`
+    volumeEl.innerHTML = `<p>${inputEl.value} gallons = ${volumeResultGallons.toFixed(3)} liters</p> 
+                          <p>${inputEl.value} liters = ${volumeResultLiters.toFixed(3)} gallons</p>`
 
-    massEl.innerHTML = `<p>${inputEl.value} kilos = ${massResultPounds.toFixed(3)} pounds | ${inputEl.value} pounds = ${massResultKilos.toFixed(3)} kilos</p>`
+    massEl.innerHTML = `<p>${inputEl.value} kilos = ${massResultPounds.toFixed(3)} pounds</p>
+                        <p>${inputEl.value} pounds = ${massResultKilos.toFixed(3)} kilos</p>`
 
 })
 
